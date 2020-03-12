31a14fdbe5ce0f175babff5fd2cc4429c67bb8b0
Milan <milandahal213@gmail.com>
2020-03-12 15:42:55 -0400

    Create main.py

---
+++ Ball Thrower/UART/UART/main.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+import ev3dev.auto as ev3
+from ev3dev2.button import Button
+from ev3dev2.sound import Sound
+import time
+screen = ev3.Screen()
+btn = Button()
+sound = Sound()
+x1=10
+y1=10
+x2=60
+y2=20
+while not btn.any():
+    
+    x1=x2
+    y1=y2
+    x2+=2
+    y2+=0
+    screen.draw.rectangle((x1,y1,x2,y2), fill='black')
+    time.sleep(0.1)
+
+
+
+
